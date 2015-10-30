81e57b9135c8eb240351f935d0cf629b3e23c196
goodjob-dev <dev.goodjob@gmail.com>
2015-10-30 14:43:36 +0400

    Edits for Arshak

--- application/views/layer/landing.php
+++ application/views/layer/landing.php
@@ -1,3 +1,5 @@
+
+
 <!-- Header -->
 <header>
 	<div class="container">
@@ -15,10 +17,14 @@
 			<div id="navbar" class="navbar-collapse collapse">
 				<ul class="nav navbar-nav page-scroll">
 					<li>
-						<a href="#about-us">Մեր մասին</a>
+						<a href="#about-us">
+							Մեր մասին
+						</a>
 					</li>
 					<li>
-						<a href="#feedbacks">Կարծիքներ</a>
+						<a href="#feedbacks">
+							Կարծիքներ
+						</a>
 					</li>
 				</ul>
 			</div>
@@ -28,89 +34,226 @@
 <!-- ./End Header -->
 
 <!-- Main Section -->
-<div class="container-fluid page-block" id="main">
+<div class="container-fluid main-section page-block" id="main">
 	
-	<div class="container">
-		<div class="text-center vertical-center">
+	<div class="container text-center">
 
 			<div class="row">
-				<div class="top-logo">
+				<div class="top-logo vertical-margin">
 					<img src="images/logo-orange.png" />
 				</div>	
-				<div class="top-description heading col-xs-8 col-xs-offset-2">
+				<div class="top-description heading vertical-margin">
 					<p>Ստացեք ձեր տնտեսական ապրանքների փաթեթը մեկ կոճակի սեղմումով</p>
 				</div>
 			</div>
 
-			<div class="row">
-				<button data-toggle="modal" data-target="#order-modal" class="btn btn-success order-now">Պատվիրել հիմա</button>	
+			<div class="row order-row">
+				<p>
+					<button data-toggle="modal" data-target="#order-modal" class="btn btn-success vertical-margin order-now">
+						Պատվիրեք հիմա
+					</button>
+				</p>	
+				<p>կամ զանգահարեք՝</p>
+				<p>
+					<u class="orange">096-516-526</u>
+				</p>
 			</div>
 			
-		</div>
+			<div class="row arrow-down">
+				<img href="#about-us" src="images/arrow-down.png" class="in-page-link"/>
+			</div>
+			
 	</div>
 
 </div>
 <!-- ./End Main Section -->
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!-- About us Section -->
-<div class="container-fluid page-block" id="about-us">
+<div class="container-fluid vertical-margin page-block" id="about-us">
+
 	<div class="container">
-		<div class="text-center vertical-center">
-			<h1 class="orange body-font">Մեր Մասին</h1>
-			<div class="row">
-				<div class="col-xs-8 col-xs-offset-2">
-					<div class="row">
-						<h3 class="body-font text-left">Ի՞նչ է gShop-ը</h3>
-						<blockquote class="answer">
-							gShop-ը առաքման համակարգ է որը հնարավորություն է տալիս ստանալ 
-							ձեզ անհրաժեշտ տնտեսական ապրանքներն ընդհամենը մեկ զանգով
-						</blockquote>
-					</div>
-					<div class="row">
-						<h3 class="body-font text-left">Առցանց տարբերակ</h3>
-						<blockquote class="answer">
-							Մեր բազմաթիվ հաճախորդների խնդրանքով gShop-ը շուտով հասանելի կլինի նաև առցանց տարբերակով։<br>
-							Եթե ցանկանում եք կարող ենք <u class="orange">տեղեկացնել</u> ձեզ կայքի բացման մասին։
-							<div class="get-notified">
-								<div class="form-horizontal">
-									<div class="form-group">
-										<div class="col-xs-12 col-lg-7 top-margin">
-											<input type="e-mail" class="e-mail form-control" placeholder="Մուտքագրեք ձեր էլ. հասցեն" id="get-notified" />
-										</div>
-										<div class="col-xs-12 col-lg-5 top-margin">
-											<input type="button" class="btn btn-success send-email col-xs-12 col-lg-5" value="Ուղարկել">
-										</div>
-									</div>
-								</div>								
-							</div>
-						</blockquote>
-					</div>
-				</div>
+
+		<h1 class="orange text-center body-font">
+			Մեր Մասին
+		</h1>
+			
+	
+		<div class="col-lg-10 col-lg-offset-1">
+			
+			<div class="row text-left">
+				<h3 class="body-font">
+					Ի՞նչ է gShop-ը
+				</h3>
+				<p class="answer">
+					gShop-ը առաքման համակարգ է, որը հնարավորություն է տալիս ստանալ 
+					ձեզ անհրաժեշտ տնտեսական ապրանքներն ընդհամենը մեկ զանգով:
+				</p>
 			</div>
 			
+			<div class="row vertical-margin text-center">
+				
+				<div class="col-sm-4 stat">
+					<div class="number">500+</div>
+					<div class="title">Օգտատեր</div>
+				</div>
+				
+				<div class="col-sm-4 stat">
+					<div class="number">1000+</div>
+					<div class="title">Պատվեր ամսեկան</div>
+				</div>
+				
+				<div class="col-sm-4 stat">
+					<div class="number">100+</div>
+					<div class="title">Ամսական գրանցվող օգտատեր</div>
+				</div>
+				
+			</div>
+			
+			<div class="row">
+				<hr />
+			</div>
+		
 		</div>
+			
 	</div>
 </div>
 <!-- ./End About us Section -->
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!-- Feedbacks section -->
 
-<div class="container-fluid page-block" id="feedbacks">
-	<div class="container">
-		<div class="container text-center vertical-center">
-			<h1 class="orange body-font">Կարծիքներ</h1>
-			<div class="row">
-				<div></div>
+<div class="container-fluid vertical-margin page-block" id="feedbacks">
+	<div class="container text-center">
+		
+		<h1 class="orange body-font scale-font">
+			Կարծիքներ
+		</h1>
+		
+		<div class="col-lg-10 col-lg-offset-1">
+			
+			<div class="row feedback vertical-margin">
+				
+				<div class="image col-sm-3">
+					<img src="images/feedbacks/feedback-1.jpg" />
+				</div>
+				
+				<div class="content col-sm-9 text-left">
+					
+					<div class="text">
+						
+					</div>
+					<div class="author">
+						<em>Արթուր Խլղաթյան</em>
+					</div>
+					
+				</div>
+				
 			</div>
-			<div class="row">
-				<button data-toggle="modal" data-target="#order-modal" class="btn btn-success order-now">Պատվիրել հիմա</button>	
+			
+			<div class="row feedback vertical-margin">
+				
+				<div class="image col-sm-3">
+					<img src="images/feedbacks/feedback-2.jpg" />
+				</div>
+				
+				<div class="content col-sm-9 text-left">
+					
+					<div class="text">
+						
+					</div>
+					<div class="author">
+						<em>Արթուր Խլղաթյան</em>
+					</div>
+					
+				</div>
+				
 			</div>
+			
+			<div class="row feedback vertical-margin">
+				
+				<div class="image col-sm-3">
+					<img src="images/feedbacks/feedback-3.jpg" />
+				</div>
+				
+				<div class="content col-sm-9 text-left">
+					
+					<div class="text">
+						
+					</div>
+					<div class="author">
+						<em>Արթուր Խլղաթյան</em>
+					</div>
+					
+				</div>
+				
+			</div>
+			
+			
+			<div class="row order-row vertical-margin">
+				<p>
+					<button data-toggle="modal" data-target="#order-modal" class="btn btn-success vertical-margin order-now">
+						Պատվիրեք հիմա
+					</button>
+				</p>	
+				<p>կամ զանգահարեք՝</p>
+				<p>
+					<u class="orange">096-516-526</u>
+				</p>
+			</div>
+		
 		</div>
+		
 	</div>
 </div>
 
 <!-- ./End Feedbacks section -->
 
+
+<!-- Footer section -->
+
+<footer class="container-fluid vertical-margin">
+	<div class="container text-center">
+		&copy; gShop inc 2015. All rights reserved.<br />
+		Հեռախոս` +374-96-516-526
+	</div>
+</footer>
+
+<!-- ./End Footer section -->
 
 <!-- Order popup -->
 
@@ -118,12 +261,26 @@
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header">
-				<p class="heading modal-title">Պատվիրել</p>
+				<p class="heading modal-title">
+					Պատվիրել
+				</p>
 			</div>
 			<div class="modal-body">
-				Խնդրում ենք զանգահարել <u class="orange">096-516-526</u> համարով և մենք կառաքենք ձեր փափեթը ձեր նշած ժամին:
-
-				<p class="order-description">Բազմաթիվ հաճախորդների խնդրանքով մեր ծառայությունները շուտով հասանելի կլինեն նաև առցանց տարբերակով։ <a href="#about-us" class="orange know-more">իմանալ ավելին</a></p>
+			
+				<p class="order-info">
+					* Մենք կկապնվենք ձեզ հետ 5 րոպեի ընթացում, այլապես ձեր առաքումը կլինի <u class="orange">անվճար</u>։
+				</p>
+			
+				<form action="" method="POST">
+					<div class="form-horizontal">
+						<div class="form-group">
+							<div class="col-lg-12">
+								<input type="text" class="form-control" placeholder="Ձեր հեռախոսահամարը։" />
+								<input type="button" class="btn btn-success phone-submit" value="Պատվիրել" />
+							</div>
+						</div>
+					</div>
+				</form>
 				
 			</div>
 			<div class="modal-footer">
@@ -133,3 +290,4 @@
 		</div>
 	</div>
 </div>
+
